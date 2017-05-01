9fba995aac69d19cdd56fb9eafa7f13e57f9b91d
Nikolaj Thorup Jensen <github@nt-jensen.dk>
2017-05-01 06:19:25 +0200

    Update

--- src/visualizer.js
+++ src/visualizer.js
@@ -1,10 +1,5 @@
 visualizer = {};
 
-visualizer.startRoomVisuals = function () {
-    let firstVal = Game.cpu.getUsed();
-    _.filter(Game.creeps);
-    let secondVal = Game.cpu.getUsed();
-    let visualCpuUsage = secondVal - firstVal;
-
-    Game.rooms['E27S83'].visual.text('Visual CPU usage: ' + visualCpuUsage.toFixed(2), 1, 1, { color: 'green', font: 0.5, align: 'left' });
+visualizer.startRoomVisuals = () => {
+    
 }
