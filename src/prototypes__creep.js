9c791469be1187aa719f8e7512acb3708f73c834
Nikolaj Thorup Jensen <github@nt-jensen.dk>
2017-05-05 09:52:39 +0200

    Update prototypes must not use arrow style functions

--- src/prototypes__creep.js
+++ src/prototypes__creep.js
@@ -1,22 +1,22 @@
-Creep.prototype.takeRandomStep = () => {
+Creep.prototype.takeRandomStep = function() {
     utils.takeRandomStep(this);
 }
 
-Creep.prototype.avoidRoomEdge = () => {
+Creep.prototype.avoidRoomEdge = function() {
     utils.avoidRoomEdge(this);
 }
 
-let oldCreepWithdraw = Creep.prototype.withdraw;
-Creep.prototype.withdraw = (target, resourceType, amount) => {
-    if (target.structureType === STRUCTURE_STORAGE && resourceType === RESOURCE_ENERGY) {
-        if (this.memory.task.role === 'distributor') {
-            return oldCreepWithdraw(target, resourceType, amount);
-        } else if (this.room.energyCapacityAvailable + this.carryCapacity >= this.room.storage.store.energy) {
-            return oldCreepWithdraw(target, resourceType, amount);
-        } else {
-            return ERR_NOT_ENOUGH_RESOURCES;
-        }
-    } else {
-        return oldCreepWithdraw(target, resourceType, amount);
-    }
-}
+//let oldCreepWithdraw = Creep.prototype.withdraw;
+//Creep.prototype.withdraw = function (target, resourceType, amount) {
+//    if (target.structureType === STRUCTURE_STORAGE && resourceType === RESOURCE_ENERGY) {
+//        if (this.memory.task.role === 'distributor') {
+//            return oldCreepWithdraw(target, resourceType, amount);
+//        } else if (this.room.energyCapacityAvailable + this.carryCapacity >= this.room.storage.store.energy) {
+//            return oldCreepWithdraw(target, resourceType, amount);
+//        } else {
+//            return ERR_NOT_ENOUGH_RESOURCES;
+//        }
+//    } else {
+//        return oldCreepWithdraw(target, resourceType, amount);
+//    }
+//}
