bde859ad4afe6596ddd72baded8da1968d83e0ec
Nikolaj Thorup Jensen <github@nt-jensen.dk>
2017-05-05 09:30:44 +0200

    Added check if storage has energy to Creep.withdraw

--- src/prototypes__creep.js
+++ src/prototypes__creep.js
@@ -5,3 +5,18 @@ Creep.prototype.takeRandomStep = () => {
 Creep.prototype.avoidRoomEdge = () => {
     utils.avoidRoomEdge(this);
 }
+
+let oldCreepWithdraw = Creep.prototype.withdraw;
+Creep.prototype.withdraw = (target, resourceType, amount) => {
+    if (target.structureType === STRUCTURE_STORAGE && resourceType === RESOURCE_ENERGY) {
+        if (this.memory.task.role === 'distributor') {
+            return oldCreepWithdraw(target, resourceType, amount);
+        } else if (this.room.energyCapacityAvailable + this.carryCapacity >= this.room.storage.store.energy) {
+            return oldCreepWithdraw(target, resourceType, amount);
+        } else {
+            return ERR_NOT_ENOUGH_RESOURCES;
+        }
+    } else {
+        return oldCreepWithdraw(target, resourceType, amount);
+    }
+}
