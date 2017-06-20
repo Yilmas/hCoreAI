ff99061dbb7b249fc67a8976063e2011b1e505bd
Nikolaj Thorup Jensen <github@nt-jensen.dk>
2017-06-20 22:44:12 +0200

    Whoops

--- src/require.js
+++ src/require.js
@@ -6,19 +6,16 @@ require('utils');
 require('prototypes__creep');
 
 // load memory
-require('brain.memory');
 
 require('brain.memory.manager');
 
 // load brain modules
 require('brain.spawn.config');
 require('brain.spawn.manager');
-require('brain.creepSpawner');
 
 require('brain.roles');
 
 require('brain.structureManager');
-require('brain.special.creepSpawner');
 
 require('brain.labManager');
 
